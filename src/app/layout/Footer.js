b9a98796361b3597745bb48b4c48b4fdc1779aea
Daffa Kurnia Fatah <daffa.kurnia@lndata.com>
2023-06-12 07:41:45 +0700

    feat: :sparkles: Add copyright

--- src/app/layout/Footer.js
+++ src/app/layout/Footer.js
@@ -11,92 +11,98 @@ const caveatFont = Caveat({
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="container">
-        <div className="row">
-          <div className="mb-3 col-lg-4 col-md-6">
-            <a className={`footer__brand ${caveatFont.className}`} href="#">
-              CandleCraft
-            </a>
-            <Paragraph level={1}>
-              Export {`CandleCraft's`} premium handcrafted candles. Captivating
-              scents, stunning designs. Enchanting glow for global markets.
-            </Paragraph>
-            <Paragraph level={1}>
-              Perum. Gading Fajar 1 B6/21 Buduran Sidoarjo,
-              <br />
-              East Java - 61252
-              <br />
-              Indonesia
-            </Paragraph>
-            <div className="d-flex mt-3">
-              <Link className="footer__link" href="tel:6285156317473">
-                <BsTelephone size={30} className="me-3" />
-              </Link>
-              <Link
-                className="footer__link"
-                href="https://wa.me/6285156317474"
-                target="_blank"
-              >
-                <BsWhatsapp size={30} className="me-3" />
-              </Link>
-              <Link
-                className="footer__link"
-                href="mailto:daffakurniaf11@gmail.com"
-              >
-                <BsEnvelope size={30} className="me-3" />
-              </Link>
+    <>
+      <footer className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="mb-3 col-lg-4 col-md-6">
+              <a className={`footer__brand ${caveatFont.className}`} href="#">
+                CandleCraft
+              </a>
+              <Paragraph level={1}>
+                Export {`CandleCraft's`} premium handcrafted candles.
+                Captivating scents, stunning designs. Enchanting glow for global
+                markets.
+              </Paragraph>
+              <Paragraph level={1}>
+                Perum. Gading Fajar 1 B6/21 Buduran Sidoarjo,
+                <br />
+                East Java - 61252
+                <br />
+                Indonesia
+              </Paragraph>
+              <div className="d-flex mt-3">
+                <Link className="footer__link" href="tel:6285156317473">
+                  <BsTelephone size={30} className="me-3" />
+                </Link>
+                <Link
+                  className="footer__link"
+                  href="https://wa.me/6285156317474"
+                  target="_blank"
+                >
+                  <BsWhatsapp size={30} className="me-3" />
+                </Link>
+                <Link
+                  className="footer__link"
+                  href="mailto:daffakurniaf11@gmail.com"
+                >
+                  <BsEnvelope size={30} className="me-3" />
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className="mb-3 col-lg-1 d-none d-lg-block"></div>
-          <div className="mb-3 col-md-3">
-            <Heading level={6} className={"mb-4"}>
-              Products
-            </Heading>
-            <ul className="footer__list">
-              <li>
-                <Link href="/collection">Scented Bubble Candle</Link>
-              </li>
-              <li>
-                <Link href="/collection">Scented Sachet Candle</Link>
-              </li>
-              <li>
-                <Link href="/collection">Glass Candle</Link>
-              </li>
-              <li>
-                <Link href="/collection">Concrete Candle</Link>
-              </li>
-              <li>
-                <Link href="/collection">Rose Candle</Link>
-              </li>
-              {/* <li>
+            <div className="mb-3 col-lg-1 d-none d-lg-block"></div>
+            <div className="mb-3 col-md-3">
+              <Heading level={6} className={"mb-4"}>
+                Products
+              </Heading>
+              <ul className="footer__list">
+                <li>
+                  <Link href="/collection">Scented Bubble Candle</Link>
+                </li>
+                <li>
+                  <Link href="/collection">Scented Sachet Candle</Link>
+                </li>
+                <li>
+                  <Link href="/collection">Glass Candle</Link>
+                </li>
+                <li>
+                  <Link href="/collection">Concrete Candle</Link>
+                </li>
+                <li>
+                  <Link href="/collection">Rose Candle</Link>
+                </li>
+                {/* <li>
                 <Link href="/collection">Custom Candle</Link>
               </li> */}
-            </ul>
+              </ul>
+            </div>
+            <div className="mb-3 col-md-3">
+              <Heading level={6} className={"mb-4"}>
+                Links
+              </Heading>
+              <ul className="footer__list">
+                <li>
+                  <Link href="/">Home</Link>
+                </li>
+                <li>
+                  <Link href="/about">About</Link>
+                </li>
+                <li>
+                  <Link href="/collection">Collection</Link>
+                </li>
+                <li>
+                  <Link href="/contact">Contact Us</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="mb-3 col-lg-1 d-none d-lg-block"></div>
           </div>
-          <div className="mb-3 col-md-3">
-            <Heading level={6} className={"mb-4"}>
-              Links
-            </Heading>
-            <ul className="footer__list">
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/about">About</Link>
-              </li>
-              <li>
-                <Link href="/collection">Collection</Link>
-              </li>
-              <li>
-                <Link href="/contact">Contact Us</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="mb-3 col-lg-1 d-none d-lg-block"></div>
         </div>
-      </div>
-    </footer>
+        <Paragraph level={2} className={"text-center pt-5 mb-0"}>
+          @2023 CandleCraft. All rights reserved
+        </Paragraph>
+      </footer>
+    </>
   );
 }
 
