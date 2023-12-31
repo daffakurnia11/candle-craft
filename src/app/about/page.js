500fe5a33da1267fa2c0da2505555a0774cf2275
Daffa Kurnia Fatah <daffa@itsavirus.com>
2023-12-31 16:00:06 +0700

    feat: :sparkles: Add our vision section

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,13 @@
 import OurValues from "../home/OurValues";
 import Header from "./Header";
+import OurVision from "./OurVision";
 
 export default function About() {
   return (
     <>
       <Header />
       <OurValues />
+      <OurVision />
     </>
   );
 }
