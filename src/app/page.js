b5912eb5872e18f43be4d102c2122a3798e490b7
Daffa Kurnia Fatah <daffa@itsavirus.com>
2024-01-03 15:18:29 +0700

    feat: :sparkles: Add new section called Materials

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Header from "@/app/home/Header";
 import Products from "@/app/home/Products";
 import OurValues from "@/app/home/OurValues";
 import Location from "@/app/home/Location";
+import Materials from "./home/Materials";
 
 export const metadata = {
   title: "The Best Exporter of Scented Candle - Home | CandleCraft",
@@ -14,6 +15,7 @@ export default function Home() {
       <Header />
       <OurValues />
       <Products />
+      <Materials />
       <Location />
       {/* <Testimonials /> */}
     </>
